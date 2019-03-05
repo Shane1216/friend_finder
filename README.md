805c5c705e10bf0d3e0e1c84ecbc3cefb80884a1
Shane <shanehill16@gmail.com>
2019-03-05 18:34:13 -0500

    Final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # friend_finder
+
+Friend Finder is an application that matches two users based upon their answers to a ten question survey. The questions attempt to gauge the users personality with questions on a scale of 1 to 5 that judges their feelings toward a certain topic. The application then compares the answers of the current user to all the other users and matches them with the most compatible user.
---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+var friends = [
+    {
+      name: "John",
+      photo: "https://ichef.bbci.co.uk/images/ic/960x540/p01br0nw.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+        name: "Paul",
+        photo: "https://e.snmc.io/i/300/w/684f84f7fb8ecebe866b2f9496d4350f/1188523",
+        scores: [
+          "3",
+          "2",
+          "4",
+          "5",
+          "2",
+          "3",
+          "1",
+          "5",
+          "3",
+          "2"
+        ]
+      },
+      {
+        name: "George",
+        photo: "http://www.georgeharrison.com/wp-content/uploads/2017/08/fender-george-feature-image-smaller-810x527.jpg",
+        scores: [
+          "5",
+          "5",
+          "3",
+          "4",
+          "5",
+          "2",
+          "3",
+          "2",
+          "4",
+          "4"
+        ]
+      },
+      {
+        name: "Ringo",
+        photo: "https://thenypost.files.wordpress.com/2015/07/gettyimages-3230856.jpg?quality=90&strip=all",
+        scores: [
+          "1",
+          "4",
+          "5",
+          "2",
+          "1",
+          "5",
+          "4",
+          "4",
+          "1",
+          "5"
+        ]
+      }
+  ];
+  
+  module.exports = friends;
